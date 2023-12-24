6688e36ef2f988d3c33ed43ec0e6bbe128e88c0b
Subhamagrahari <shubhamagrahari72844@gmail.com>
2023-12-24 18:14:58 +0530

    added validation

--- src/Components/Thala/Thala.jsx
+++ src/Components/Thala/Thala.jsx
@@ -1,43 +1,60 @@
 import React, { useState } from 'react'
 import './Thala.css'
 import Validation from '../Validation'
+import reason1 from '../Asset/reason1.jpeg'
 const Thala = () => {
-    const[values,setValues]=useState({
-        name:''
-    })
-    const[errors,setError]=useState({})
-    const handleSubmit=(event)=>{
-        event.preventDefault();
-        setError(Validation(values));
-      
-        }
-
-
-        const handlechange=(event)=>{
-            setValues(prev=>({...prev, [event.target.name]:[event.target.value]}))
-        }
-
-
-  return (
-   <div className="thala">
-    <div className="content">
-    <h1>Behind Every<span>TH</span>ing there is <span>A LA</span>w😍</h1>
-    </div>
-    <div className="container">
-        <form action="" onSubmit={handleSubmit}>
-        <div className="inputs">
-            <input type="text" placeholder="Enter Anything.." onChange={handlechange}/>
-            <button type='submit'>Check</button>
-        </div>
-        </form>
-      
-        <div>
+    const style={
+        position:"relative",
+        top:"242px",
+        height:"140px",
+        width:"180px",
         
-         {errors.name && <span>{errors.name}</span>}
+
+    }
+    const [values, setValues] = useState({
+        name: ''
+    })
+    const [errors, setError] = useState({ name: "" })
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        setError(prev => ({ ...prev, name: Validation(values) }))
+        // setError(Validation(values));
+        //    setError(errors:)
+    }
+
+
+    const handlechange = (event) => {
+        setValues(prev => ({ ...prev, name: event.target.value }))
+    }
+
+
+    return (
+        <div className='main'>
+            <div className="thala">
+                <div className="content">
+                    <h1>Behind Every<span>TH</span>ing there is  <span>A LA</span>w😍</h1>
+                </div>
+                <div className="container">
+                    <form action="" onSubmit={handleSubmit}>
+                        <div className="inputs">
+                            <input type="text" placeholder="Enter Anything.." name="name" onChange={handlechange} />
+
+                            <button type='submit' >Check</button>
+                        </div>
+                    </form>
+
+
+                    {values.name === "" ? <div></div> : <div className='explanation'>{values.name} {errors.name && <span>{errors.name}</span>}
+                    </div>}
+
+
+                </div>
+
+            </div>
+           {errors.name=="=7 THALA for reason"?<div className='poster-image'><img src={reason1} alt="" style={style} /></div>:<div></div>}
         </div>
-    </div>
-   </div>
-  )
+    )
 }
 
 export default Thala
--- src/Components/Validation.js
+++ src/Components/Validation.js
@@ -1,13 +1,40 @@
+ function digitsum(num)
+ {
+  var sum=0;
+  while(num)
+  {
+    let re=num%10;
+    sum=sum+re;
+    num=Math.floor(num/10);
+  }
+  return sum;
+ }
+ 
+ 
+ 
  function Validation(values){
   let reason="";
-  if(values.name.length<=7)
+  if(values.name.length<7)
   {
-    reason= "no thala for you";
+    let result= digitsum(values.name);
+    if(result===7)
+    {
+      reason="=7 THALA for reason"
+    }
+    else{
+      reason="No thala for you"
+    }
   }
-  else{
-    reason="";
+  else if(values.name.length===7){
+    reason="=7 THALA for reason";
   }
-  
+  else if(values.name===7){
+    reason="=7 THALA for reason";
+  }
+ else
+ {
+  reason="";
+ }
   return reason;
  }
 
